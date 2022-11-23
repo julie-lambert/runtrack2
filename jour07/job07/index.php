c76318ec97b2e8badf0c5c63ebb480b6790dd8bb
julie-lambert <julie.lambert@laplateforme.io>
2022-11-23 10:19:35 +0100

    rendu debut job07

---
+++ jour07/job07/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form method="get">
+     Texte<br>
+    <input type="text" name="str">
+    <br><br>
+    <select name="fonction" id="fonction">
+            <option value="">--Please choose an option--</option>
+            <option value="gras">Gras</option>
+            <option value="cesar">Cesar</option>
+            <option value="plateforme">Plateforme</option>
+    </select>
+
+    <input type="submit" value="Envoyer">
+</form>
+
+
+</body>
+</html>
